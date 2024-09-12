52f0f2be6921555fdc3123bad9e62591deedcb2b
rofidamedhat <141781059+rofidamedhat@users.noreply.github.com>
2024-09-13 00:11:03 +0300

    continuing set up app

---
+++ lib/travel_app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TravelApp extends StatelessWidget {
+  const TravelApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp();
+  }
+}
